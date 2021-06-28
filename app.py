5b2b72c2f19868319001d8c92bb3142a4f78d55f
s1f10210310 <85666719+s1f10210310@users.noreply.github.com>
2021-06-28 21:13:33 +0900

    re

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ kinoko_count = 3
 takenoko_count = 5
 messages = ['Kinoko is wonderful!', 'Takenoko is awesome!']
 
+import re
+
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
@@ -26,6 +28,11 @@ def answer():
     message_html = ''
     for i in range(len(messages)):
         message = messages[i]
+        message = re.sub(r'&', r'&amp;', message)
+        message = re.sub(r'<', r'&lt;', message)
+        message = re.sub(r'<', r'&gt;', message)
+        message = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', message)
+        message = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', message)
         message_html += '<div class="alart {1}" role="alart">{0}</div>\n'.format(message, 'alart-warning ms=5' if i % 2 == 0 else 'alart-success me-5')
 
     return render_template('vote.html', **vars())
